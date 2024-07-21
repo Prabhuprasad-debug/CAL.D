f05a548b77f4190ecc8cc1061140adadcc8b290a
Prabhuprasad-debug <prabhuprasad4428@gmail.com>
2024-07-21 18:58:39 +0530

    first commit
    Date: 21-07-2024

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var input = document.getElementById('input'), // input/output button
+    number = document.querySelectorAll('.numbers div'), // number buttons
+    operator = document.querySelectorAll('.operators div'), // operator buttons
+    result = document.getElementById('result'), // equal button
+    clear = document.getElementById('clear'), // clear button
+    resultDisplayed = false; // flag to keep an eye on what output is displayed
+
+// adding click handlers to number buttons
+
+
+// on click of 'equal' button
+
+
+// clearing the input on press of clear
